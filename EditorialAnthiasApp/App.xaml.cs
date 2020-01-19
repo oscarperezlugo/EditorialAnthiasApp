20865ee5ca79dafcf87ebfc5777cd6d5665384fe
oscarperezlugo <operezlugoes@gmail.com>
2020-01-19 22:09:11 +0100

    Initial commit

---
+++ EditorialAnthiasApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EditorialAnthiasApp
+{
+    public partial class App : Application
+    {
+        
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTc5NTI2QDMxMzcyZTMzMmUzMFZGMzZoRVFzR0R3QUYzS3pUYm5Zd0VwdkdFa0lxNHlSRHVDeFlsNFNQKzQ9");
+            InitializeComponent();
+
+            MainPage = new ReadPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ EditorialAnthiasApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EditorialAnthiasApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (!DesignMode.IsDesignModeEnabled)
+                ((MyCompassViewModel)BindingContext).StartCommand.Execute(null);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (!DesignMode.IsDesignModeEnabled)
+                ((MyCompassViewModel)BindingContext).StopCommand.Execute(null);
+        }
+    }
+}
